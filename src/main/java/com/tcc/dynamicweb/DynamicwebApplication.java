e5abbcde650acdca84fb350c7404186ed5fa7988
CarlosDevUnifal <125224606+CarlosDevUnifal@users.noreply.github.com>
2023-10-10 19:26:49 -0300

    primeiro commit, projeto cru

---
+++ src/main/java/com/tcc/dynamicweb/DynamicwebApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.dynamicweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcc/dynamicweb/DynamicwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.dynamicweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
